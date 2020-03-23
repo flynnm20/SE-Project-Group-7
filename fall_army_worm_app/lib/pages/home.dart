2f45c868f2ba16fbc967b554876f49f5eaa4412e
Brian Farrell <55883565+branflakes6@users.noreply.github.com>
2020-03-23 04:40:08 +0000

    Changed default location to Dublin

--- fall_army_worm_app/lib/pages/home.dart
+++ fall_army_worm_app/lib/pages/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomePage extends StatefulWidget {
   @override
+
   State<StatefulWidget> createState() {
     return MyAppState();
   }
@@ -13,13 +15,14 @@ class MyAppState extends State<HomePage> {
 
   GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final LatLng _center = const LatLng(53.3498, -6.2603);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       theme: ThemeData(
           primarySwatch: Colors.grey,
