a3173ac76fa50b0efd05b5a70e4adc561df9574d
EazyEgan <35083316+EazyEgan@users.noreply.github.com>
2020-04-15 21:45:59 +0100

    added gps mvement functionality #powerhouse

--- fall_army_worm_app/lib/pages/home.dart
+++ fall_army_worm_app/lib/pages/home.dart
@@ -11,15 +11,72 @@ class HomePage extends StatefulWidget {
 }
 
 
-class MyAppState extends State<HomePage> {
 
+
+
+
+class MyAppState extends State<HomePage> {
+  final Map<String, Marker> _markers = {};
   GoogleMapController mapController;
 
   final LatLng _center = const LatLng(53.3498, -6.2603);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+
+
   }
+
+updateLocation() async{
+
+  var currentLocation = await Geolocator()
+      .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+  mapController.animateCamera(
+    CameraUpdate.newCameraPosition(
+      CameraPosition(
+          target: LatLng(currentLocation.latitude, currentLocation.longitude), zoom: 15.0),
+    ),
+  );
+  setState(() {
+    _markers.clear();
+    final marker = Marker(
+      markerId: MarkerId("curr_loc"),
+      position: LatLng(currentLocation.latitude, currentLocation.longitude),
+      infoWindow: InfoWindow(title: 'Your Location'),
+
+    );
+    _markers["Current Location"] = marker;
+
+  });
+
+}
+
+
+  void _getLocation() async {
+
+    var currentLocation = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+
+    setState(() {
+      _markers.clear();
+      CameraPosition(
+        target: LatLng(currentLocation.latitude, currentLocation.longitude),
+        zoom: 11.0,
+      );
+      final marker = Marker(
+        markerId: MarkerId("curr_loc"),
+        position: LatLng(currentLocation.latitude, currentLocation.longitude),
+        infoWindow: InfoWindow(title: 'Your Location'),
+
+      );
+      _markers["Current Location"] = marker;
+
+    });
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
 
@@ -30,13 +87,29 @@ class MyAppState extends State<HomePage> {
             title: TextStyle(color: Colors.white),
           )),
       home: Scaffold(
+       /* floatingActionButton: FloatingActionButton(
+          onPressed: _getLocation,
+          tooltip: 'Get Location',
+          child: Icon(Icons.flag),
+        ),*/
+        floatingActionButton:
+
+        FloatingActionButton(
+            child: Icon(Icons.location_on),
+            onPressed: () {
+          updateLocation();
+        }
+
+        ),
 
         body: GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
             target: _center,
-            zoom: 11.0,
+            zoom: 0.0,
           ),
+
+          markers: _markers.values.toSet(),
         ),
 
       ),
